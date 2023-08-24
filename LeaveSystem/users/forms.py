8179714795aa93c502f0f764560c548994ae7ef4
Muchiri-cmd <davismuchiri21@gmail.com>
2023-08-24 08:49:49 +0300

    first-commit

---
+++ LeaveSystem/users/forms.py
@@ -0,0 +1,16 @@
+#forms for the user app
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserAddForm(UserCreationForm):
+    "Extends user creation form with other fields"
+    email=forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'eg.davis@gmail.com'}))
+
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2']
+
+class UserLogin(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'password'}))
