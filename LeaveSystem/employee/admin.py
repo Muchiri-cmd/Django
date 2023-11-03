e5fed7e2622e457929f75096a6ad32407bf78d0d
Muchiri-cmd <107479427+Muchiri-cmd@users.noreply.github.com>
2023-11-03 19:20:54 +0300

    Delete Project Leave System

--- LeaveSystem/employee/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from employee.models import Role,Department,Employee
-
-
-# Register your models here.
-admin.site.register(Role)
-admin.site.register(Department)
-
-admin.site.register(Employee)
